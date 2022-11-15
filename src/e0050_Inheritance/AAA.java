9c8d883ac9fcd5e5f6a6675c46dc01312e81d6b1
voyagerdva <mariner-9@yandex.ru>
2022-11-15 14:46:33 +0300

    Initial commit

---
+++ src/e0050_Inheritance/AAA.java
@@ -0,0 +1,10 @@
+package e0050_Inheritance;
+
+public class AAA {
+    int i;
+    int j;
+
+    void show_ij() {
+        System.out.println("i & j: " + i + " " + j);
+    }
+}
---
+++ src/e0050_Inheritance/BBB.java
@@ -0,0 +1,13 @@
+package e0050_Inheritance;
+
+public class BBB extends AAA {
+    int k;
+
+    void show_k() {
+        System.out.println("k: " + k);
+    }
+
+    void summ() {
+        System.out.println("i+j+k: " + (i+j+k));
+    }
+}
